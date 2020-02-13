a34317a8d6ab69c4f9d6272cf6741e555c0b47cb
yamady0704 <fleis_preis@yahoo.co.jp>
2020-02-13 09:21:21 +0900

    initial commit

---
+++ src/app/components/movie-list/movie-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"]
+})
+export class MovieListComponent implements OnInit {
+  movies: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.movies = [
+      {
+        Title: "Starwars: Goretech",
+        Year: "2018",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
+      },
+      {
+        Title: "Starwars: Goretech",
+        Year: "2018",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
+      },
+      {
+        Title: "Starwars: Goretech",
+        Year: "2018",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
+      }
+    ];
+  }
+}
